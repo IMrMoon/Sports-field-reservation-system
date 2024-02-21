e8a8c890c94d8e8751afda7f24f119faefc5a6e6
Segev <sgvchen12@gmail.com>
2024-02-21 20:51:48 +0200

    add some function declaration in Player and manager

--- Field.cpp
+++ Field.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Field.h"
+
+
+void Field::addRate(float player_rate) {
+    if (numRates == capacity)
+    {
+        int new_capacity;
+        if(capacity == 0)
+            new_capacity = 1;
+        else
+            new_capacity = capacity * 2;
+        float * newRates = new float [new_capacity];
+
+        for (int i = 0; i < numRates; ++i) {
+            newRates[i] = rates[i];
+        }
+
+        delete [] rates;
+        rates = newRates;
+        capacity = new_capacity;
+    }
+    rates[numRates++] = player_rate;
+    cout << "Rating added successfully." << endl;
+}
