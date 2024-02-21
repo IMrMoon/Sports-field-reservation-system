1f6321ee505f774f44fa665f7ed594c430f52765
Segev <sgvchen12@gmail.com>
2024-02-21 21:46:33 +0200

    add some function for every class for a good start we have a problem in orders class

--- Orders.cpp
+++ Orders.cpp
@@ -1,16 +1,16 @@
+////
+//// Created by Segev on 2/21/2024.
+////
 //
-// Created by Segev on 2/21/2024.
+//#include "Orders.h"
 //
-
-#include "Orders.h"
-
-Orders::Orders(const string &orderId, const Date &orderDate, time_t orderTime) : order_id(""),
-                                                                                 order_date(Date()),
-                                                                                 order_time(0) {}
-
-void Orders::print_order() {
-        cout << "Order ID: " << order_id << endl;
-        cout << "Order Date: " << order_date << endl; // Assuming Date class has a toString() function
-        cout << "Order Time: " << ctime(&order_time); // Convert time_t to string and print
-}
-
+//Orders::Orders(const string &orderId, const Date &orderDate, time_t orderTime) : order_id(""),
+//                                                                                 order_date(Date()),
+//                                                                                 order_time(0) {}
+//
+//void Orders::print_order() {
+//        cout << "Order ID: " << order_id << endl;
+//        cout << "Order Date: " << order_date << endl; // Assuming Date class has a toString() function
+//        cout << "Order Time: " << ctime(&order_time); // Convert time_t to string and print
+//}
+//
--- Orders.h
+++ Orders.h
@@ -10,33 +10,33 @@
 
 
 class Orders {
-private:
-    string order_id;
-    Date order_date;
-    time_t order_time;
-public:
-    Orders(const string &orderId, const Date &orderDate, time_t orderTime);
-    Orders() : order_id(""), order_date(Date()), order_time(0) {
-        // Default constructor implementation
-    }
-    virtual ~Orders();
-    Orders(const Orders& other_order){
-        this->order_id = other_order.order_id;
-        this->order_date = other_order.order_date;
-        this->order_time = other_order.order_time;
-    }
-
-    const string &getOrderId() const {return order_id;}
-
-    const Date &getOrderDate() const {return order_date;}
-
-    time_t getOrderTime() const {return order_time;}
-    void setOrderId(const string &orderId) {order_id = orderId;}
-
-    void setOrderDate(const Date &orderDate) {order_date = orderDate;}
-
-    void setOrderTime(time_t orderTime) {order_time = orderTime;}
-    void print_order();
+//private:
+//    string order_id;
+//    Date order_date;
+//    time_t order_time;
+//public:
+//    Orders(const string &orderId, const Date &orderDate, time_t orderTime);
+//    Orders() : order_id(""), order_date(Date()), order_time(0) {
+//        // Default constructor implementation
+//    }
+//    virtual ~Orders();
+//    Orders(const Orders& other_order){
+//        this->order_id = other_order.order_id;
+//        this->order_date = other_order.order_date;
+//        this->order_time = other_order.order_time;
+//    }
+//
+//    const string &getOrderId() const {return order_id;}
+//
+//    const Date &getOrderDate() const {return order_date;}
+//
+//    time_t getOrderTime() const {return order_time;}
+//    void setOrderId(const string &orderId) {order_id = orderId;}
+//
+//    void setOrderDate(const Date &orderDate) {order_date = orderDate;}
+//
+//    void setOrderTime(time_t orderTime) {order_time = orderTime;}
+//    void print_order();
 };
 
 
